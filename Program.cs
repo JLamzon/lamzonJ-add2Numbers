1f55a3407dc79cab32643271b9528965a292cc70
JLamzon <jlamzon@yahoo.com>
2022-10-21 11:05:32 -0700

    added peer review to program

--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@
 //GitHubChallenge - Redo all these mini Challenges
 //Adding 2 numbers + data validation and option to play again
 
+// Peer Review: Isaiah Keith Ferguson - Great and simple 
+// program the best use of the try.parse i've seen for this challenge. 
+// The only thing I might change is expanding the scope from 1-100 to maybe 
+// the length of an int but other than that i really like the game! 
+
 
 
 
